af955ec5ed5e08afcc7dd8c65e0f7e60a4eb91c6
Nguyen Thien Hung <ngthienhung277@users.noreply.github.com>
2025-04-14 16:56:50 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+})
 // const CONNECTION_URL = 'mongodb+srv://hungnuen7:hungnuen277@cluster0.5pzsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const PORT = process.env.PORT || 5000;
